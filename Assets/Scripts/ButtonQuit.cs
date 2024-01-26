511e6c7e1570df85a199e66ed55e986196dfbee1
Randy Riley <randy@rileyid.com>
2024-01-26 12:53:08 -0500

    Data Persistence with JSON added
    Quit button on Main play screen not working currently

--- Assets/Scripts/ButtonQuit.cs
+++ Assets/Scripts/ButtonQuit.cs
@@ -6,23 +6,27 @@ public class ButtonQuit : MonoBehaviour
 {
     public Material rollOverMat;
     public Material buttonMat;
+    public StartMenuManager StartMenuManager;
 
     private void OnMouseEnter()
     {
-        gameObject.GetComponent<Renderer>().material = rollOverMat;
+      //  gameObject.GetComponent<Renderer>().material = rollOverMat;
+      // MADE A ROLLOVER SCRIPT FOR BOTH BALLS FOR FLASHING
     }
     private void OnMouseExit()
     {
-        gameObject.GetComponent<Renderer>().material = buttonMat;
+      //  gameObject.GetComponent<Renderer>().material = buttonMat;
     }
     private void OnMouseDown()
     {
-        Debug.Log("You Hit Quit");
+        StartMenuManager.QuitGame();
+       // Debug.Log("You Hit Quit");
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartMenuManager = StartMenuManager.GetComponent<StartMenuManager>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/StartMenuManager.cs
+++ Assets/Scripts/StartMenuManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;//FOR SAVING DATA TO DISK
 using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class StartMenuManager : MonoBehaviour
 {
     public Button StartButton;
@@ -12,7 +16,50 @@ public class StartMenuManager : MonoBehaviour
     public TextMeshProUGUI HighScoreText;
     public GameManager gameManager;
 
+    ///FOR SAVING BETWEEN SESSIONS
+    [System.Serializable]//required for JSON to save file
+    class HighScoreClass //Its a good idea to make a small class with just what is needed before saving JSON file
+    {
+        public string userName;
+        public int highScore;
+        public string highScoreUser; 
+    }
+ 
+    private void Awake()
+    {
+        LoadData();
+    }
 
+    public void SaveData()
+    {
+        HighScoreClass data = new HighScoreClass();
+        data.highScore = GameManager.highScore;
+        data.highScoreUser = GameManager.highScoreUser;
+        data.userName = GameManager.userName;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadData()
+    {
+
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            HighScoreClass data = JsonUtility.FromJson<HighScoreClass>(json);
+
+            GameManager.userName = data.userName;
+            GameManager.highScore = data.highScore;
+            GameManager.highScoreUser = data.highScoreUser;
+            
+        }
+
+
+
+    }
     public void LoadGameScene()
     {
         if (NameInput.text == "Enter Your Name")
@@ -24,8 +71,15 @@ public class StartMenuManager : MonoBehaviour
     }
     public void QuitGame()
     {
-        Debug.Log("You Hit Quit");
+        SaveData();
+       //THIS ALLOW YOU TO TEST QUITTING ALONG WITH THE NAMESPACE DECLARATION PART
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
+
 
     // Start is called before the first frame update
     void Start()
@@ -45,11 +99,6 @@ public class StartMenuManager : MonoBehaviour
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-      
-    }
 }
 //CODE GRAVEYARD//Should have worked the first time, but that would be too easy////
 // Debug.Log(GameManager.Instance.GetComponent<GameManager>().userName);
